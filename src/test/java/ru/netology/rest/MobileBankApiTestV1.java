4a1f1ab58b69d94c02738e248c8ddd714d88ebaa
Ярослав Черников <yaroslavcher@gmail.com>
2022-08-12 20:27:06 +0300

    body tests OK

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 class MobileBankApiTestV1 {
     @Test
@@ -11,13 +13,41 @@ class MobileBankApiTestV1 {
       // Given - When - Then
       // Предусловия
       given()
-          .baseUri("http://localhost:9999/api/v1")
+          .baseUri("http://localhost:9999/api/v1/")
       // Выполняемые действия
       .when()
-          .get("/demo/accounts")
+          .get("demo/accounts")
       // Проверки
       .then()
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+    @Test
+    void shouldReturnBodySize() {
+        given()
+                .baseUri("http://localhost:9999/api/v1/")
+                .when()
+                .get("demo/accounts")
+                .then()
+                .body("", hasSize(3));
+    }
+    @Test
+    void shouldReturnCurrencyRUB() {
+        given()
+                .baseUri("http://localhost:9999/api/v1/")
+                .when()
+                .get("demo/accounts")
+                .then()
+                .body("[0].currency",equalTo("RUB"));
+    }
+    @Test
+    void shouldReturnCurrencyUSD() {
+        given()
+                .baseUri("http://localhost:9999/api/v1/")
+                .when()
+                .get("demo/accounts")
+                .then()
+                .body("[1].currency",equalTo("USD"));
     }
 }
